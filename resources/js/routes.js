2b14718eaf8fd84dc1012f0b2c27994c560ce359
kstar0101 <canopus0410@gmail.com>
2021-11-01 16:23:41 +0900

    first commit

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,6 +1,6 @@
 export default [
     { path: '/dashboard', component: require('./components/Dashboard.vue').default },
-    { path: '/dashboard', component: require('./components/Dashboard.vue').default },
+    // { path: '/dashboard', component: require('./components/Dashboard.vue').default },
     { path: '/profile', component: require('./components/Profile.vue').default },
     { path: '/developer', component: require('./components/Developer.vue').default },
     { path: '/users', component: require('./components/Users.vue').default },
